f9f7e2b7480b8c1f7468da7d3d60c0463921194f
shaik <shaikayaaz50@gmail.com>
2023-02-10 18:10:34 +0530

    something

---
+++ src/EvenOdd.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class EvenOdd{
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        int y = 425;
+
+        int sum = x + y;
+
+        System.out.println("Sum is " + sum);
+
+        if(sum > y){
+            System.out.println("This is incorrect");
+        } else {
+            System.out.println("This is correct");
+        }
+
+
+    }
+}
